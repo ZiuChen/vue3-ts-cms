5e95392829cb85c0bc4450236ca6279203a940f1
ZiuChen <ZiuChen@outlook.com>
2022-05-29 15:31:52 +0800

    build(vue.config.js .eslintrc.js): 自定义webpack配置的方法 忽略eslint的报错

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+// 如果我们不满足Vue CLI为我们提供的webpack配置，可以进行自定义
+const path = require('path')
+module.exports = {
+  // // 配置方式1: Vue CLI 提供的属性
+  // outputDir: './build',
+  // // 配置方式2: 与webpack的属性完全一致，最后会进行合并
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: '@/component'
+  //     }
+  //   }
+  // }
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     components: '@/components'
+  //   }
+  // }
+  // 配置方式3: 链式编程
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('components', '@/components')
+  }
+}
