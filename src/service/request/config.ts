d55bd43ae9922791c9efe32ee6c25e9cb2227467
ZiuChen <ZiuChen@outlook.com>
2022-05-31 11:10:44 +0800

    ci: 调整环境变量的key与value 根据条件设置环境变量的值

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -1,14 +1,11 @@
-// let BASE_URL = ''
-// let BASE_NAME = ''
-// if (process.env.NODE_ENV === 'development') {
-//   BASE_URL = 'https://ZiuChen.org/dev'
-//   BASE_NAME = 'ZiuChen'
-// } else if (process.env.NODE_ENV === 'production') {
-//   BASE_URL = 'https://ZiuChen.org/prod'
-//   BASE_NAME = 'ZiuChen'
-// } else if (process.env.NODE_ENV === 'test') {
-//   BASE_URL = 'https://ZiuChen.org/test'
-//   BASE_NAME = 'ZiuChen'
-// }
+let BASE_URL = ''
+const TIME_OUT = 10000
+if (process.env.NODE_ENV === 'development') {
+  BASE_URL = 'http://123.207.32.32:8000/'
+} else if (process.env.NODE_ENV === 'production') {
+  BASE_URL = 'https://ZiuChen.org/prod'
+} else if (process.env.NODE_ENV === 'test') {
+  BASE_URL = 'https://ZiuChen.org/test'
+}
 
-// export { BASE_URL, BASE_NAME }
+export { BASE_URL, TIME_OUT }
