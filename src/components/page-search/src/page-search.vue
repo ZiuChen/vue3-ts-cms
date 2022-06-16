5ea03f53d22586f61d2c92aefc18b63f101d410e
ZiuChen <457353192@qq.com>
2022-06-16 21:31:41 +0800

    feat(components): 封装公共组件`PageSearch`
    对`ZUForm`进行二次封装 加入了header与footer插槽 根据FormConfig动态决定绑定的formData都有哪些属性

---
+++ src/components/page-search/src/page-search.vue
@@ -0,0 +1,62 @@
+<template>
+  <ZUForm v-model="formData" v-bind="searchFormConfig">
+    <template #header>
+      <h2>高级检索</h2>
+    </template>
+    <template #footer>
+      <div class="btn-group">
+        <el-button @click="restore">重置</el-button>
+        <el-button @click="search" type="primary">搜索</el-button>
+      </div>
+    </template>
+  </ZUForm>
+  {{ formData }}
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+import type { PropType } from 'vue'
+import ZUForm from '@/base-ui/form'
+import type { IFormConfig } from '@/base-ui/form/types'
+import type { IFormData } from '../types'
+
+export default {
+  props: {
+    searchFormConfig: {
+      type: Object as PropType<IFormConfig>,
+      reuqired: true
+    }
+  },
+  components: {
+    ZUForm
+  },
+  setup(props) {
+    // TODO: 用reactive替换ref会导致: 数据变了但是template未变
+    const formData = ref<IFormData>({})
+    const formItems = ref(props.searchFormConfig?.formItems ?? [])
+    const mapFormItems = (formData: IFormData) => {
+      for (const { field } of formItems.value) {
+        formData[field] = ''
+      }
+    }
+    const restore = () => mapFormItems(formData.value)
+    const search = () => {
+      console.log('search')
+    }
+    mapFormItems(formData.value)
+    return {
+      formData,
+      restore,
+      search
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.btn-group {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+</style>
