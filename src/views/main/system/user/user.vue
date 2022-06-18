4a0bd66e3b0026fbd836ddbfe65eb408968bcf22
ZiuChen <457353192@qq.com>
2022-06-19 00:02:53 +0800

    feat(user.vue): 请求用户列表数据并使用ZUTable简单展示
    还有待做进一步的封装

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,22 +1,47 @@
 <template>
   <div class="user">
     <PageSearch :searchFormConfig="searchFormConfig" />
+    <ZUTable :listData="userList" :propList="propList"></ZUTable>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import PageSearch from '@/components/page-search'
+import { defineComponent, computed } from 'vue'
+import { useStore } from '@/store'
 import { searchFormConfig } from './config/search.config'
+import PageSearch from '@/components/page-search'
+import ZUTable from '@/base-ui/table'
 
 export default defineComponent({
   name: 'user',
   components: {
-    PageSearch
+    PageSearch,
+    ZUTable
   },
   setup() {
+    const store = useStore()
+    store.dispatch('system/getPageListAction', {
+      pageUrl: '/users/list',
+      queryInfo: {
+        offset: 0,
+        size: 10
+      }
+    })
+    const userList = computed(() => store.state.system.userList)
+    const userCount = computed(() => store.state.system.userCount)
+    const propList = [
+      { prop: 'name', label: '用户名', minWidth: '100' },
+      { prop: 'realname', label: '真实姓名', minWidth: '100' },
+      { prop: 'cellphone', label: '手机号码', minWidth: '100' },
+      { prop: 'enable', label: '状态', minWidth: '100' },
+      { prop: 'createAt', label: '创建时间', minWidth: '250' },
+      { prop: 'updateAt', label: '更新时间', minWidth: '250' }
+    ]
     return {
-      searchFormConfig
+      searchFormConfig,
+      userList,
+      userCount,
+      propList
     }
   }
 })
