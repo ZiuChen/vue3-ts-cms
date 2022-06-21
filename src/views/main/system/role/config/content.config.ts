8504f9b4042058b7e32eac2fbb3b5be9c86fc15b
ZiuChen <457353192@qq.com>
2022-06-21 14:38:35 +0800

    feat(role.vue): 配置角色管理页面

---
+++ src/views/main/system/role/config/content.config.ts
@@ -0,0 +1,26 @@
+export const contentTableConfig = {
+  title: '角色列表',
+  propList: [
+    { prop: 'name', label: '角色名', minWidth: '100' },
+    { prop: 'intro', label: '权限介绍', minWidth: '100' },
+    {
+      prop: 'createAt',
+      label: '创建时间',
+      minWidth: '100',
+      slotName: 'createAt'
+    },
+    {
+      prop: 'updateAt',
+      label: '更新时间',
+      minWidth: '100',
+      slotName: 'updateAt'
+    },
+    {
+      label: '操作',
+      minWidth: '100',
+      slotName: 'handler'
+    }
+  ],
+  showIndexColumn: true,
+  showSelectColumn: true
+}
--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="role">
-    <h2>role</h2>
+    <PageSearch :searchFormConfig="searchFormConfig"></PageSearch>
+    <PageContent
+      :pageName="'role'"
+      :contentTableConfig="contentTableConfig"
+    ></PageContent>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import PageSearch from '@/components/page-search'
+import PageContent from '@/components/page-content'
+import { searchFormConfig } from './config/search.config'
+import { contentTableConfig } from './config/content.config'
 
 export default defineComponent({
   name: 'role',
+  components: {
+    PageSearch,
+    PageContent
+  },
   setup() {
-    return {}
+    return {
+      searchFormConfig,
+      contentTableConfig
+    }
   }
 })
 </script>
