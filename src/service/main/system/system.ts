6f0418e8d285d4794352ec111e0b6db65a0f14ae
ZiuChen <457353192@qq.com>
2022-06-24 19:44:31 +0800

    feat(system.ts): 增加加载动画
    showLoading

--- src/service/main/system/system.ts
+++ src/service/main/system/system.ts
@@ -5,6 +5,7 @@ import type { IListResult } from './type'
 export function getPageListData(url: string, queryInfo: any) {
   return ZURequest.post<IDataType<IListResult>>({
     url: url,
-    data: queryInfo
+    data: queryInfo,
+    showLoading: true
   })
 }
